c8035ec4b2cb12de0194d8bc749f15b68c72cd69
Nilesh <nileshtailor@topsinfosolutions.com>
2022-06-20 17:28:01 +0530

    create routs for login and registration vue side
    create routs for login and registration vue side

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//import HelloWorld from '@/components/HelloWorld'
+import Home from '@/component/home'
+import Login from '@/component/Login'
+import Register from '@/component/Register'
+import Profile from '@/component/Profile'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+    /*{
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }*/
+     {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
     }
   ]
 })
